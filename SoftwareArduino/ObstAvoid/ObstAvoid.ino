07ecebcc93cf82b5ada04d53b19ac9fdf152105a
JoaoVictorGordo <gordo.batista.joao@gmail.com>
2016-10-25 15:28:26 -0200

    added 3rd party library references

--- SoftwareArduino/ObstAvoid/ObstAvoid.ino
+++ SoftwareArduino/ObstAvoid/ObstAvoid.ino
@@ -1,4 +1,28 @@
 // TODO referenciar direitinho as referencias
+// TODO add clear function
+
+
+/*                      RELATED TO PWM LIBRARY USAGE 
+Copyright (c) 2012 Sam Knight
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+/*                      RELATED TO RF24 LIBRARY USAGE 
+ Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+
+  This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+    version 2 as published by the Free Software Foundation.
+*/
+
+
+
 
 /* nRF2L01+ pins
    1 - GND
@@ -16,7 +40,7 @@
 
 
 #include <SPI.h>
-#include <nRF24L01.h>
+#include <nRF24L01.h> // acho que não está sendo utilizada!!!
 #include <RF24.h>
 #include <Time.h>
 #include <PWM.h>
@@ -129,7 +153,7 @@ struct sensor_t
 
 bool safety_button;
 
-bool select = 1; // 0: master(remote control) ; 1: slave(robot) 
+    bool select = 1; // 0: master(remote control) ; 1: slave(robot) 
 
 short int *T;                  // Truth Table
 
@@ -254,6 +278,10 @@ void setup()
 
     T = (short int*)malloc(32*sizeof(short int));
     initTable(T); // T: truth table
+
+    // to avoid first mean values to be absurd take a few samples from the environment before running
+    for(int i = 0; i < 10; i++)
+        SensorReading();
 }
 
 void loop()
